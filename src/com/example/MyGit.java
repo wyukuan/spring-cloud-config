d5a53ef226e0e2625ea879f94da6f3f0445198d7
wyukuan <19919882174@163.com>
2019-09-26 11:27:33 +0800

    这是我的第2次提交

--- src/com/example/MyGit.java
+++ src/com/example/MyGit.java
@@ -3,6 +3,6 @@ package com.example;
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("hello");
-        
+        System.out.println("java");
     }
 }
